9da7b66374fa7cbc021250c76e72ad3dbc27b00f
felabel <felicityabel2016@gmail.com>
2024-07-24 03:46:06 +0100

    completed the pagination tasks in python

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+ a function named index_range that takes two integer arguments page and page_size
+"""
+
+from typing import Tuple
+def index_range(page: int, page_size:int) -> Tuple[int, int]:
+    """
+    Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return(start, end)
